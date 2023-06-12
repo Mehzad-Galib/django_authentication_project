93f77da0d14fcbaa7f77ea4d83ce44973b4ce31e
Mehzad Galib <m.galib.rajjo15@gmail.com>
2023-06-13 00:33:50 +0600

    first commit

---
+++ first_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('', views.home, name='homepage'),
+    path('logout/', views.user_logout, name='logout'),
+    path('pass_change/', views.pass_change, name='passchange'),
+    path('update_profile/', views.update_profile, name='update_profile'),
+]
